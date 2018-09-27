d2e2ca79743b4214147ac233530ae972f608b7cf
Flavio Farias <ffpsj@ic.ufal.br>
2018-09-27 04:40:30 -0300

    create ui components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import Top from './components/Top';
+import Content from './components/Content';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Top />
+        <Content name='Flavio Farias' email='flavio_farias_@hotmail.com' picture='assets/img/1.jpg' />
       </div>
     );
   }
---
+++ src/components/Top.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Top extends Component {
+    render() {
+        return (
+            <div className='d-flex align-items-center highlight-blue'>
+                <div className='container'>
+                    <div className='intro'>
+                        <h2 className='text-center'>Facebook Login ReactJS</h2>
+                        <p className='text-center'>To get started, authenticate with Facebook</p>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Top;
